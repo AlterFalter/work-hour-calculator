19248578a1e3f8f67c5aa3c3b74f4d8384141bd7
Yannis Kraemer <yannkra21@gmail.com>
2021-08-24 21:26:22 +0200

    Add ProfileOverviewWindow.

---
+++ WorkHourCalculator/Windows/ProfileOverviewWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using WorkHourCalculator.Models;
+
+namespace WorkHourCalculator.Windows
+{
+    /// <summary>
+    /// Interaction logic for ProfileOverviewWindow.xaml
+    /// </summary>
+    public partial class ProfileOverviewWindow : Window
+    {
+        private readonly Account account;
+
+        public ProfileOverviewWindow(Account account, Profile editingProfile)
+        {
+            InitializeComponent();
+            this.account = account;
+            DataContext = this.account;
+        }
+
+        private void AddNewProfile_Click(object sender, RoutedEventArgs e)
+        {
+            this.account.AddProfile();
+        }
+
+        private void EditSelectedProfile_Click(object sender, RoutedEventArgs e)
+        {
+            EditProfileWindow editProfileWindow = new EditProfileWindow(this.account);
+            editProfileWindow.Show();
+
+            this.Close();
+        }
+
+        private void SelectProfile_Click(object sender, RoutedEventArgs e)
+        {
+            this.account.Save();
+
+            MainWindow mainWindow = new MainWindow(this.account);
+            mainWindow.Show();
+
+            this.Close();
+        }
+    }
+}
