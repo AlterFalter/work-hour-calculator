b208c994a1dd44db36d5e0faee941e03b77d93eb
Yannis Kraemer <yannkra21@gmail.com>
2021-08-24 21:27:14 +0200

    Add WindowsUserConfiguration.

---
+++ WorkHourCalculator/Models/WindowsUserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using WorkHourCalculator.Helper;
+
+namespace WorkHourCalculator.Models
+{
+	public class WindowsUserConfiguration : Configuration<WindowsUserConfiguration>
+    {
+		public string LastUsedFilepath
+		{
+			get => this.ReadField(nameof(this.LastUsedFilepath));
+			set
+			{
+				this.AddOrSetField(nameof(this.LastUsedFilepath), value);
+			}
+		}
+    }
+}
